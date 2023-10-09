0a58a8d32c64e8abbd2816b97e3f47889d941749
WENTE <manqing321@gmail.com>
2023-10-09 22:39:27 +0800

    add 20 answer

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,7 +3,10 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+fn call_me(){
+    println!("less is more");
+}
 
 fn main() {
     call_me();
